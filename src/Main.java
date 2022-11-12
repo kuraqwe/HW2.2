5ef57e272178a054651ca27973cc13254d4d94d2
danya <kuraqwee@gmail.com>
2022-11-12 13:31:01 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var a = 78.2;
-        var b = 82.7;
-        System.out.println(b - a);
-        System.out.println(b  % a);
+        System.out.println("Всего работников в компании " + 640/8 + " человек.");
     }
 }
