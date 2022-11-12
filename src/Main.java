056f1ed1df2b8966328c7aa1887b6807c5f3d459
danya <kuraqwee@gmail.com>
2022-11-12 13:27:59 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var a = 78.2;
         var b = 82.7;
-        System.out.println(a + b);
         System.out.println(b - a);
+        System.out.println(b  % a);
     }
 }
