82dbce56b689aa82879382748fe9d48acba14c6e
danya <kuraqwee@gmail.com>
2022-11-12 13:24:28 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var a = 78.2;
+        var b = 82.7;
+        System.out.println(a + b);
+        System.out.println(b - a);
+    }
+}
